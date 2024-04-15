205e4f1751bf8294e963e9b6f93f26d2aca2f5c6
Martins2405 <miguelmartinssilva24@gmail.com>
2024-04-15 13:40:47 -0300

    arrumando pastas

---
+++ Aplicativo de Lista de Tarefas/windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\migma\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\migma\\Aplicativo de Lista de Tarefas\\To-do-list\\Aplicativo de Lista de Tarefas" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\migma\\flutter"
+  "PROJECT_DIR=C:\\Users\\migma\\Aplicativo de Lista de Tarefas\\To-do-list\\Aplicativo de Lista de Tarefas"
+  "FLUTTER_ROOT=C:\\Users\\migma\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\migma\\Aplicativo de Lista de Tarefas\\To-do-list\\Aplicativo de Lista de Tarefas\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\migma\\Aplicativo de Lista de Tarefas\\To-do-list\\Aplicativo de Lista de Tarefas"
+  "FLUTTER_TARGET=C:\\Users\\migma\\Aplicativo de Lista de Tarefas\\To-do-list\\Aplicativo de Lista de Tarefas\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ==,RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC9lNzZjOTU2NDk4ODQxZTFhYjQ1ODU3N2QzODkyMDAzZTU1M2U0ZjNjLw=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\migma\\Aplicativo de Lista de Tarefas\\To-do-list\\Aplicativo de Lista de Tarefas\\.dart_tool\\package_config.json"
+)
